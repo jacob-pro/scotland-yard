0a5620196033d17b14b0979b128dbd5f71bc8822
jacob <jacob@JACOB-PC>
2019-04-04 02:28:21 +0100

    begin multiplayer

---
+++ src/main/java/uk/ac/bris/cs/scotlandyard/MLGMain.java
@@ -0,0 +1,22 @@
+package uk.ac.bris.cs.scotlandyard;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import uk.ac.bris.cs.scotlandyard.ui.Utils;
+import uk.ac.bris.cs.scotlandyard.ui.controller.MLGGame;
+
+public final class MLGMain {
+
+	public static void main(String[] args) {
+		JFXApp.launch(JFXApp.class, args);
+	}
+
+	public static final class JFXApp extends Application {
+
+		@Override public void start(Stage stage) {
+			Thread.currentThread().setUncaughtExceptionHandler((thread, throwable) -> Utils.handleFatalException(throwable));
+			new MLGGame(Utils.setupResources(), stage);
+		}
+	}
+
+}
---
+++ src/main/java/uk/ac/bris/cs/scotlandyard/ui/controller/MLGGame.java
@@ -0,0 +1,60 @@
+package uk.ac.bris.cs.scotlandyard.ui.controller;
+
+import javafx.scene.Scene;
+import javafx.scene.control.MenuItem;
+import javafx.stage.Stage;
+import uk.ac.bris.cs.scotlandyard.ResourceManager;
+import uk.ac.bris.cs.scotlandyard.model.Spectator;
+import uk.ac.bris.cs.scotlandyard.ui.model.BoardProperty;
+import uk.ac.bris.cs.scotlandyard.ui.model.ModelProperty;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import static uk.ac.bris.cs.scotlandyard.ui.Utils.handleFatalException;
+
+public class MLGGame extends BaseGame implements Spectator {
+
+	public MLGGame(ResourceManager manager, Stage stage) {
+		super(manager, stage, new BoardProperty());
+		stage.setTitle("MLG ScotlandYard");
+		stage.setScene(new Scene(this.root()));
+		stage.getIcons().add(manager.getImage(ResourceManager.ImageResource.ICON));
+		stage.show();
+	}
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+
+		//We need to add some buttons here
+//		MenuItem newGame = new MenuItem("New game");
+//		newGame.setOnAction(e -> LocalGame.newGame(resourceManager, new Stage(), false));
+//		addMenuItem(newGame);
+//
+//		MenuItem showTests = new MenuItem("Restart in test capture mode");
+//		showTests.setOnAction(e -> {
+//			getStage().close();
+//			LocalGame.newGame(resourceManager, new Stage(), true);
+//		});
+//		addMenuItem(showTests);
+
+		setupGame();
+	}
+
+	private void setupGame() {
+		StartScreen startScreen = new StartScreen(resourceManager, config, this::createGame);
+		showOverlay(startScreen.root());
+	}
+
+	private void createGame(ModelProperty setup) {
+		hideOverlay();
+		try {
+			throw new RuntimeException();
+			//MLGGame.Game game = new MLGGame.Game(setup);
+		} catch (Exception e) {
+			e.printStackTrace();
+			handleFatalException(e);
+		}
+
+	}
+}
